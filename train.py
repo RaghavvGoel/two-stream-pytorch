b76f6affdbf321ce2a7c478c7efccb8e85b18b37
Raghavvgoel <raghavvg@andrew.cmu.edu>
2022-05-21 14:27:02 -0400

    just started

---
+++ train.py
@@ -0,0 +1,30 @@
+import numpy as np 
+import torch 
+import torch.nn as nn
+from two_stream import TwoStream
+
+class Trainer(TwoStream):
+
+    def __init__():
+
+        # define optimizer 
+        # define scheduler 
+
+
+    def find_loss():
+        pass 
+
+
+
+def main():
+    
+    # load data 
+    
+    pass
+
+
+
+
+if __name__  == "__main__":
+
+    main()
---
+++ two_stream.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+import torch
+import math
+import collections
+import numpy as np
+
+# class Spatial():
+#     # bunch of conv layers, separate class will allow easy modification (training separately)
+#     pass
+
+# class Temporal():
+#     # bunch of conv layers, separate class will allow easy modification (training separately)
+#     pass
+
+class TwoStream():
+    
+    def __init__(self) -> None:
+        
+        self.spatial = nn.Sequential(nn.Conv2d()
+
+        )
+
+    pass
+
+    def find_loss():
+        pass
+
